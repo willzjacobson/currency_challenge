37dc523e47878d4c6f5378e7bc95bd5c1f0774cf
Will Jacobson <willjacobson1@gmail.com>
2018-10-13 16:27:49 -0700

    test

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/prop-types': ['off'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', { allow: ['info', 'error'] }],
+  },
+};
