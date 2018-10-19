23b7fc66d11e8ec0dea0015bbb314113eb8d7418
Will Jacobson <willjacobson1@gmail.com>
2018-10-19 11:21:50 -0700

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: './lib/client/index.js',
+  entry: ['@babel/polyfill', './lib/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js',
